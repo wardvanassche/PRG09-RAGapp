6c1766c937173fb8c200188ebe1d586f256c6b38
Ward van Assche <1060834@hr.nl>
2025-11-10 16:50:09 +0100

    Initial commit

---
+++ CLIENT/src/App.jsx
@@ -0,0 +1,9 @@
+import './app.css'
+
+export default function App() {
+    return (
+        <div className="flex h-screen items-center justify-center bg-gray-500 text-white text-4xl font-bold">
+            Programmeren 9
+        </div>
+    );
+}
