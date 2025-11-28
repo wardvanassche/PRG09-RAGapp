d99a83677a9acca7b76a27bf80eed8c29465b063
Ward van Assche <1060834@hr.nl>
2025-11-28 12:46:30 +0100

    Fixes

--- SERVER/embed.js
+++ SERVER/embed.js
@@ -3,28 +3,29 @@ import {FaissStore} from "@langchain/community/vectorstores/faiss";
 import {OllamaEmbeddings} from "@langchain/ollama";
 import {RecursiveCharacterTextSplitter} from "@langchain/textsplitters";
 
+export async function creatingVectorStore() {
+    console.log("Creating FAISS vector store...");
 
 // load
-const loader = new PDFLoader("./assets/context/cursuswijzer.pdf");
-const docs = await loader.load();
-
+    const loader = new PDFLoader("./assets/context/cursuswijzer.pdf");
+    const docs = await loader.load();
 
 // split
-const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-});
-const splitDocs = await splitter.splitDocuments(docs);
-
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const splitDocs = await splitter.splitDocuments(docs);
 
 // embedding model
-const embeddings = new OllamaEmbeddings({
-    model: process.env.OLLAMA_EMBEDDING_MODEL, // or any model you have pulled in Ollama
-    baseUrl: process.env.OLLAMA_BASE_URL, // Default Ollama URL
-});
+    const embeddings = new OllamaEmbeddings({
+        model: process.env.OLLAMA_EMBEDDING_MODEL, // or any model you have pulled in Ollama
+        baseUrl: process.env.OLLAMA_BASE_URL, // Default Ollama URL
+    });
 
+// embed and create vector store
+    const vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
+    await vectorStore.save("./faiss-index");
 
-// create vector store
-const vectorStore = await FaissStore.fromDocuments(splitDocs, embeddings);
-await vectorStore.save("./faiss-index");
-console.log("FAISS vector store saved to ./faiss-index");
+    console.log("FAISS vector store saved to ./faiss-index");
+}
--- SERVER/index.js
+++ SERVER/index.js
@@ -1,68 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import {ChatOllama, OllamaEmbeddings} from "@langchain/ollama";
-import {AIMessage, HumanMessage, SystemMessage} from "@langchain/core/messages";
-import {FaissStore} from "@langchain/community/vectorstores/faiss";
+import express from 'express';
+import cors from 'cors';
+import chatRoute from './chat.js';
+import {creatingVectorStore} from "./embed.js";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const model = new ChatOllama({
-    model: process.env.OLLAMA_LLM_MODEL, // or any model you have pulled in Ollama
-    baseUrl: process.env.OLLAMA_BASE_URL, // Default Ollama URL
-});
-
-
-// embedding model
-const embeddings = new OllamaEmbeddings({
-    model: process.env.OLLAMA_EMBEDDING_MODEL, // or any model you have pulled in Ollama
-    baseUrl: process.env.OLLAMA_BASE_URL, // Default Ollama URL
-});
+// Creating embeddings and vector store
+await creatingVectorStore();
 
 app.get('/', (req, res) => {
     res.json({message: 'Server is running'})
 })
 
-app.post("/chat", async (req, res) => {
+app.use('/chat', chatRoute)
 
-    // get prompt from request body
-    const prompt = req.body.prompt;
-    console.log("Received prompt:", prompt);
-
-    // get history from request body
-    const history = req.body.history;
-    console.log("Received history:", history);
-
-    // get context from vectordata
-    const vectorStore = await FaissStore.load("faiss-index", embeddings);
-    // get relevant context from vector data (based on human prompt)
-    const relevantDocs = await vectorStore.similaritySearch(prompt, 3);
-    const contextText = relevantDocs.map(d => d.pageContent).join("\n---\n")
-
-    // define messages
-    let messages = [
-        new SystemMessage('Je bent een behulpzame assistent'),
-        new HumanMessage(`Hier is wat context:\n${contextText}\n\nVraag: ${prompt}`),
-    ];
-
-    for (const {human, ai} of history) {
-        messages.push(new HumanMessage(human))
-        messages.push(new AIMessage(ai))
-    }
-
-    messages.push(new HumanMessage(prompt))
-
-    console.log(messages);
-
-    const stream = await model.stream(messages);
-    res.setHeader('Content-Type', 'text/plain');
-    for await (const chunk of stream) {
-        console.log(chunk.content)
-        res.write(chunk.content)
-    }
-    res.end()
-});
-
-app.listen(3000, () => console.log(`Server running on http://localhost:3000`))
+app.listen(3000, () =>
+    console.log(`Server running on http://localhost:3000`)
+);
